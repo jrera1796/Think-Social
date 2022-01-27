f0ffc32a0956dfe68b96b0608c446b890b105ddd
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-01-26 23:36:04 -0800

    Finished routes, all routes tested and functional

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/think-social', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
